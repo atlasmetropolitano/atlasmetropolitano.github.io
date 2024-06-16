43751a3e8234efcb15a38f38dfd428e30717083d
atlasmetropolitano <52287587+atlasmetropolitano@users.noreply.github.com>
2024-06-16 10:54:08 -0300

    Initial commit

--- js/scripts_distribucion_espacial.js
+++ js/scripts_distribucion_espacial.js
@@ -1,22 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const yearButtons = document.querySelectorAll('.year-btn');
-    const mapFrame = document.getElementById('map-frame');
-    const yearText = document.getElementById('year-text');
+function changeMap(year) {
+    const iframe = document.getElementById('map-frame');
+    const mapTitle = document.getElementById('map-title');
+    const mapDescription = document.getElementById('map-description');
+    const yearImage = document.getElementById('year-image');
 
-    yearButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            // Remover clase 'active' de todos los botones
-            yearButtons.forEach(btn => btn.classList.remove('active'));
-            // Añadir clase 'active' al botón clicado
-            button.classList.add('active');
+    iframe.src = `Densidad/${year}/index.html`;
+    yearImage.src = `img/${year}.jpg`;
+    yearImage.alt = `Densidad de Población ${year}`;
 
-            // Cambiar el texto del año
-            const selectedYear = button.getAttribute('data-year');
-            yearText.textContent = selectedYear;
+    switch (year) {
+        case '1991':
+            mapTitle.innerText = 'Densidad de Población 1991';
+            mapDescription.innerText = 'Densidad de Población de Buenos Aires de 1991 a 2001...';
+            break;
+        case '2001':
+            mapTitle.innerText = 'Densidad de Población de 2001';
+            mapDescription.innerText = 'Densidad de Población de población de Buenos Aires de 1991 a 2022...';
+            break;
+        case '2010':
+            mapTitle.innerText = 'Densidad de Población de 2010';
+            mapDescription.innerText = 'Densidad de Población de población de Buenos Aires de 2001 a 2010...';
+            break;
+        case '2022':
+            mapTitle.innerText = 'Densidad de Población de 2022';
+            mapDescription.innerText = 'Densidad de Población de población de Buenos Aires de 2010 a 2022...';
+            break;
+    }
 
-            // Cambiar la fuente del iframe del mapa según el año seleccionado
-            mapFrame.src = `${selectedYear}/${selectedYear}.html`;
-        });
-    });
-});
+    const buttons = document.querySelectorAll('.year-button');
+    buttons.forEach(button => button.classList.remove('active'));
 
+    document.querySelector(`.year-button[onclick="changeMap('${year}')"]`).classList.add('active');
+}

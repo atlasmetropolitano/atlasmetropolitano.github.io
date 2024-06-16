c1f5ca1ade3f0b1728aae74f6284a8bde992b604
atlasmetropolitano <52287587+atlasmetropolitano@users.noreply.github.com>
2024-06-15 22:14:44 -0300

    Initial commit

---
+++ Percentual/1991_2001/layers/layers.js
@@ -0,0 +1,43 @@
+var wms_layers = [];
+
+
+        var lyr_googlesastellite_0 = new ol.layer.Tile({
+            'title': 'google sastellite',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}'
+            })
+        });
+var format_4122_Variacin_porcentual_1991_2001_1 = new ol.format.GeoJSON();
+var features_4122_Variacin_porcentual_1991_2001_1 = format_4122_Variacin_porcentual_1991_2001_1.readFeatures(json_4122_Variacin_porcentual_1991_2001_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_4122_Variacin_porcentual_1991_2001_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_4122_Variacin_porcentual_1991_2001_1.addFeatures(features_4122_Variacin_porcentual_1991_2001_1);
+var lyr_4122_Variacin_porcentual_1991_2001_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_4122_Variacin_porcentual_1991_2001_1, 
+                style: style_4122_Variacin_porcentual_1991_2001_1,
+                popuplayertitle: "4122_Variación_porcentual_1991_2001",
+                interactive: true,
+    title: '4122_Variación_porcentual_1991_2001<br />\
+    <img src="styles/legend/4122_Variacin_porcentual_1991_2001_1_0.png" /> -2 - 11<br />\
+    <img src="styles/legend/4122_Variacin_porcentual_1991_2001_1_1.png" /> 11 - 36<br />\
+    <img src="styles/legend/4122_Variacin_porcentual_1991_2001_1_2.png" /> 36 - 60<br />\
+    <img src="styles/legend/4122_Variacin_porcentual_1991_2001_1_3.png" /> 60 - 100<br />\
+    <img src="styles/legend/4122_Variacin_porcentual_1991_2001_1_4.png" /> 100 - 195<br />'
+        });
+
+lyr_googlesastellite_0.setVisible(true);lyr_4122_Variacin_porcentual_1991_2001_1.setVisible(true);
+var layersList = [lyr_googlesastellite_0,lyr_4122_Variacin_porcentual_1991_2001_1];
+lyr_4122_Variacin_porcentual_1991_2001_1.set('fieldAliases', {'ID': 'ID', 'IN1': 'IN1', 'PROV': 'PROV', 'PAIS': 'PAIS', 'AMBA': 'AMBA', 'NOMPART': 'NOMPART', 'CODCIM': 'CODCIM', 'CODINDEC': 'CODINDEC', 'CODPBA': 'CODPBA', 'REFERENCIA': 'REFERENCIA', 'AREA': 'AREA', 'PERIMETRO': 'PERIMETRO', 'COLOR': 'COLOR', 'GNA': 'GNA', 'NAM': 'NAM', 'FNA': 'FNA', 'SAG': 'SAG', 'Field': 'Field', 'Area23': 'Area23', 'IDJUR': 'IDJUR', 'Partido': 'Partido', 'P1991': 'P1991', 'P2022': 'P2022', 'Variación': 'Variación', 'ObjectID': 'ObjectID', });
+lyr_4122_Variacin_porcentual_1991_2001_1.set('fieldImages', {'ID': 'TextEdit', 'IN1': 'TextEdit', 'PROV': 'TextEdit', 'PAIS': 'TextEdit', 'AMBA': 'TextEdit', 'NOMPART': 'TextEdit', 'CODCIM': 'TextEdit', 'CODINDEC': 'TextEdit', 'CODPBA': 'TextEdit', 'REFERENCIA': 'TextEdit', 'AREA': 'TextEdit', 'PERIMETRO': 'TextEdit', 'COLOR': 'TextEdit', 'GNA': 'TextEdit', 'NAM': 'TextEdit', 'FNA': 'TextEdit', 'SAG': 'TextEdit', 'Field': 'TextEdit', 'Area23': 'TextEdit', 'IDJUR': 'TextEdit', 'Partido': 'TextEdit', 'P1991': 'TextEdit', 'P2022': 'TextEdit', 'Variación': 'TextEdit', 'ObjectID': 'Range', });
+lyr_4122_Variacin_porcentual_1991_2001_1.set('fieldLabels', {'ID': 'hidden field', 'IN1': 'hidden field', 'PROV': 'hidden field', 'PAIS': 'hidden field', 'AMBA': 'hidden field', 'NOMPART': 'hidden field', 'CODCIM': 'hidden field', 'CODINDEC': 'hidden field', 'CODPBA': 'hidden field', 'REFERENCIA': 'hidden field', 'AREA': 'hidden field', 'PERIMETRO': 'hidden field', 'COLOR': 'hidden field', 'GNA': 'hidden field', 'NAM': 'hidden field', 'FNA': 'hidden field', 'SAG': 'hidden field', 'Field': 'hidden field', 'Area23': 'hidden field', 'IDJUR': 'hidden field', 'Partido': 'inline label - always visible', 'P1991': 'inline label - always visible', 'P2022': 'inline label - always visible', 'Variación': 'inline label - always visible', 'ObjectID': 'hidden field', });
+lyr_4122_Variacin_porcentual_1991_2001_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ Percentual/1991_2001/styles/4122_Variacin_porcentual_1991_2001_1_style.js
@@ -0,0 +1,61 @@
+var size = 0;
+var placement = 'point';
+
+var style_4122_Variacin_porcentual_1991_2001_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Variación");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= -2.495294 && value <= 10.847804) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,245,235,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 10.847804 && value <= 36.062205) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(253,210,165,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 36.062205 && value <= 60.304395) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(253,146,67,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 60.304395 && value <= 100.244826) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(223,80,5,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 100.244826 && value <= 194.956906) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(127,39,4,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
